c1a5dde4f7b3c512dceb8c99e2c8393d2a7a4cf2
Merek Hailey <mhailey@harding.edu>
2024-02-22 08:44:32 -0600

    Polished UI and Progress Bar

--- src/Components/ProgressBar.jsx
+++ src/Components/ProgressBar.jsx
@@ -8,10 +8,13 @@ export default function TotalProgressBar(props) {
 
 
   return (
-        <ProgressBar className='progressBar'>
-            <ProgressBar variant="success" now={props.numCompleted} min={0} max={100} key={1} />
+    <div className='progressBar'>
+        <h1>Total Progress</h1>
+        <ProgressBar>
+            <ProgressBar variant="success" now={props.numCompleted} min={0} max={100} label={`${props.numCompleted}%`} key={1} />
             <ProgressBar variant="warning" now={props.numPossible} min={0} max={100} key={2} />
             <ProgressBar animated variant="info" now={props.numPotential} min={0} max={100} key={3} />
         </ProgressBar>
+    </div>
   );
 }
